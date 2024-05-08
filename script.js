de7a4bda3e7aa2321a21c6dc301c5e32ecae39bb
Jack L <jackktlam@gmail.com>
2024-05-08 11:55:43 +0100

    sellWeapon function

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ function buyWeapon() {
             currentWeapon ++;
             goldText.innerText = gold;
             let newWeapon = weapons[currentWeapon].name;
-            text.innerText = `You now have a ${newWeapon}`;
+            text.innerText = `You now have a ${newWeapon}.`;
             inventory.push(newWeapon);
             text.innerText += ` In your inventory you have: ${inventory}`;
         } else {
@@ -116,6 +116,18 @@ function buyWeapon() {
         text.innerText = "You already have the most powerful weapon!";
         button2.innerText = "Sell weapon for 15 gold";
         button2.onclick = sellWeapon;
+    }
+}
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold
+        let toSell = inventory.shift();
+        text.innerText = `${toSell} was sold.`;
+        text.innerText += ` In your inventory you have: ${inventory}`;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
